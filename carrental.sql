195bf9c1a49af347151097c7f7727b119182493f
H K Charan <hkcharan08@gmail.com>
2024-12-06 10:53:48 +0530

    commit

---
+++ carrental.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2024 at 06:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carrental`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+-- Error reading structure for table carrental.bookings: #1932 - Table 'carrental.bookings' doesn't exist in engine
+-- Error reading data for table carrental.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`bookings`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+-- Error reading structure for table carrental.brands: #1932 - Table 'carrental.brands' doesn't exist in engine
+-- Error reading data for table carrental.brands: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`brands`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+-- Error reading structure for table carrental.cars: #1932 - Table 'carrental.cars' doesn't exist in engine
+-- Error reading data for table carrental.cars: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`cars`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+-- Error reading structure for table carrental.contact: #1932 - Table 'carrental.contact' doesn't exist in engine
+-- Error reading data for table carrental.contact: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`contact`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table carrental.payments: #1932 - Table 'carrental.payments' doesn't exist in engine
+-- Error reading data for table carrental.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+-- Error reading structure for table carrental.subscribers: #1932 - Table 'carrental.subscribers' doesn't exist in engine
+-- Error reading data for table carrental.subscribers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`subscribers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table carrental.users: #1932 - Table 'carrental.users' doesn't exist in engine
+-- Error reading data for table carrental.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification`
+--
+-- Error reading structure for table carrental.verification: #1932 - Table 'carrental.verification' doesn't exist in engine
+-- Error reading data for table carrental.verification: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `carrental`.`verification`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
